f29f87ce13c587482420b1cadeab3c1db5cdd2f0
René Milk <rene.milk@wwu.de>
2018-08-28 16:51:10 +0200

    [ci] ensure writable and clnflict free build dir

--- .ci/travis.script.bash
+++ .ci/travis.script.bash
@@ -4,8 +4,8 @@ set -e
 set -x
 
 BASE_DIR=${HOME}/src
-BUILD_DIR=${BASE_DIR}/build
-export PYTHONPATH=${PYTHONPATH}:${BUILD_DIR}/lib:${BASE_DIR}/src
+BUILD_DIR=/tmp/build
+export PYTHONPATH=${BUILD_DIR}/lib:${PYTHONPATH}
 
 mkdir ${BUILD_DIR} && cd ${BUILD_DIR}
 cmake ${BASE_DIR}

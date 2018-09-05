c6833af879de97d470d95689910308da36d624e3
Stephan Rave <stephanrave@uni-muenster.de>
2018-09-05 15:01:42 +0200

    [travis] fix named tuple import error

--- .ci/travis.script.bash
+++ .ci/travis.script.bash
@@ -5,7 +5,13 @@ set -x
 
 BASE_DIR=/src
 BUILD_DIR=/tmp/build
-export PYTHONPATH=${BUILD_DIR}/lib:${PYTHONPATH}
+export PYTHONPATH=${BUILD_DIR}/lib
+# Using
+#   export PYTHONPATH=${BUILD_DIR}/lib:${PYTHONPATH}
+# will fail with empty PYTHONPATH as the empty string
+# after ':' will be treated as the current directory
+# in which the operator module is imported instead of
+# the operator module from stdlib.
 
 mkdir ${BUILD_DIR} && cd ${BUILD_DIR}
 cmake ${BASE_DIR}

338204303d38d5e39a6a90a4c26582405d76f817
René Fritze <rene.fritze@wwu.de>
2019-02-27 16:41:43 +0100

    fix pymor requirement

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
     maintainer='Rene Fritze',
     maintainer_email='rene.fritze@wwu.de',
     version='0.5.0',
+    install_requires=['pymor>=0.5,<0.6'],
     package_dir={'': 'src'},
     packages=find_packages('src'),
     ext_modules=[CMakeExtension('pymor_dealii')],

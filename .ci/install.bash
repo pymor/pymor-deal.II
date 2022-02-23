f62282b8e5219d5b7c303a1b5e77215da866f43e
René Fritze <rene.fritze@wwu.de>
2022-02-23 14:36:13 +0100

    [ci] disable install test

--- .ci/install.bash
+++ .ci/install.bash
@@ -8,7 +8,6 @@ BASE_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; cd ../ ; pwd -P )"
 source ${BASE_DIR}/.ci/common_setup.bash
 
 python -m pip install .
-
 python -m pip uninstall -y pymor_dealii
 
 python -m pip install -e .
